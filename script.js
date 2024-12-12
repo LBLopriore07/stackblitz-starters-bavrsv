275a0e86ed442784e7ad81854f7c38f05c34daa1
LBLopriore07 <stu189299@lynnschools.org>
2024-12-12 12:13:53 -0500

    Commit new homepage and projects

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('Hello!');
+function todaysdate (){
+    const d=new date ();
+    return "todays date is" +d   
+}
+console.log(todaysdate())
+const body = document.body
+const d = new date();
+
+if (d.getHours() > 20 || d.gethours() < 8) {
+    body.style("backgroundColor") = "black";
+    body.style(color) = "white";
+} else {
+body.style("backgroundColor" = "whtie")
+body.style("color") = "black";
+
+}
+
+ const makeTable = () => {
+    const tableBody = document.getElemenetbytagname ("tbody") [0];
+    tableBody.interHTML = "";
+    if (Select.value = "all") {
+        
+    }
+ }
